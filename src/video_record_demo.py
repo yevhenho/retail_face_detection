1dea54add054c883a743949d0dc77a2ee2f84cfc
yevhenho <djeg@ukr.net>
2018-04-01 09:16:42 +0300

    add write output

--- src/video_record_demo.py
+++ src/video_record_demo.py
@@ -17,8 +17,8 @@ from src.models.wide_resnet import WideResNet
 
 detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
 emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
-age_gender_model_path = '../trained_models/age-gender-estimation.hdf5'
-race_model_path = '../trained_models/racenet_vgg16_25ep_seed343.h5'
+age_gender_model_path = '../trained_models/weights.18-4.06.hdf5'
+# race_model_path = '../trained_models/racenet_vgg16_25ep_seed343.h5'
 video_path = '../data/video.mp4'
 
 emotion_labels = get_labels('fer2013')
@@ -34,14 +34,14 @@ emotion_offsets = (20, 40)
 # loading models
 face_detection = load_detection_model(detection_model_path)
 emotion_classifier = load_model(emotion_model_path, compile=False)
-race_classifier = load_model(race_model_path, compile=False)
+# race_classifier = load_model(race_model_path, compile=False)
 age_gender_classifier = WideResNet(64, depth=16, k=8)()
 age_gender_classifier.load_weights(age_gender_model_path)
 
 # getting input model shapes for inference
 emotion_target_size = emotion_classifier.input_shape[1:3]
 age_gender_target_size = age_gender_classifier.input_shape[1:3]
-race_target_size = race_classifier.input_shape[1:3]
+# race_target_size = race_classifier.input_shape[1:3]
 
 # starting lists for calculating modes
 gender_window = []
@@ -50,13 +50,18 @@ emotion_window = []
 # starting video streaming
 cv2.namedWindow('window_frame')
 video_capture = cv2.VideoCapture(video_path)
-while (video_capture.isOpened()):
 
-    bgr_image = video_capture.read()[1]
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (464, 848))
+
+while (video_capture.isOpened()):
+    ret, bgr_image = video_capture.read()
+    if ret == False:
+        break
+
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
     rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
     faces = detect_faces(face_detection, gray_image)
-
     for face_coordinates in faces:
 
         x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
@@ -65,7 +70,7 @@ while (video_capture.isOpened()):
         x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
         gray_face = gray_image[y1:y2, x1:x2]
         try:
-            race_face = cv2.resize(gray_face, (race_target_size))
+            # race_face = cv2.resize(gray_face, (race_target_size))
             rgb_face = cv2.resize(rgb_face, (age_gender_target_size))
             gray_face = cv2.resize(gray_face, (emotion_target_size))
         except:
@@ -73,8 +78,8 @@ while (video_capture.isOpened()):
         gray_face = preprocess_input(gray_face, False)
         gray_face = np.expand_dims(gray_face, 0)
         gray_face = np.expand_dims(gray_face, -1)
-        race_face = np.expand_dims(race_face, 0)
-        race_face = np.expand_dims(race_face, -1)
+        # race_face = np.expand_dims(race_face, 0)
+        # race_face = np.expand_dims(race_face, -1)
 
 
         emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
@@ -85,8 +90,8 @@ while (video_capture.isOpened()):
         age_prediction = age_gender_classifier.predict(rgb_face)
         age_text = int(age_prediction[1].dot(np.arange(0, 101).reshape(101, 1)).flatten())
         gender_text = gender_labels[np.argmax(age_prediction[0][0])]
-        race_text = race_labels[np.argmax(race_classifier.predict(race_face))]
-        gender_window.append(gender_text+' '+str(age_text)+':'+race_text)
+        # race_text = race_labels[np.argmax(race_classifier.predict(race_face))]
+        gender_window.append(gender_text+' '+str(age_text)) #+':'+race_text
 
         if len(gender_window) > frame_window:
             emotion_window.pop(0)
@@ -94,21 +99,26 @@ while (video_capture.isOpened()):
         try:
             emotion_mode = mode(emotion_window)
             gender_mode = mode(gender_window)
+
         except:
             continue
 
-        if gender_text == gender_labels[0]:
-            color = (0, 0, 255)
-        else:
-            color = (255, 0, 0)
+        # if gender_text == gender_labels[0]:
+        #     color = (0, 0, 255)
+        # else:
+        color = (255, 0, 0)
 
         draw_bounding_box(face_coordinates, rgb_image, color)
         draw_text(face_coordinates, rgb_image, gender_mode,
                   color, 0, -20, 1, 1)
         draw_text(face_coordinates, rgb_image, emotion_mode,
                   color, 0, -45, 1, 1)
-
+    frame = cv2.flip(rgb_image, 0)
+    out.write(frame)
     bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
     cv2.imshow('window_frame', bgr_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+video_capture.release()
+out.release()
+cv2.destroyAllWindows()
